92aed3c2d08620c896d63c8e20563d1a6bded243
yxHenry <y18583236050@163.com>
2017-09-01 17:27:06 +0800

    c

--- ro.py
+++ ro.py
@@ -6,12 +6,46 @@ import re
 import os
 import time
 
+# proxy = {'http': '27.24.158.155:84'}
+# proxy_support = urllib.request.ProxyHandler(proxy)
+# opener = urllib.request.build_opener(proxy_support)
+# urllib.request.install_opener(opener)
+#
+# import urllib2
+# import random
+#
+#
+# def getHtml(url, proxies):
+#     random_proxy = random.choice(proxies)
+#     proxy_support = urllib2.ProxyHandler({"http": random_proxy})
+#     opener = urllib2.build_opener(proxy_support)
+#     urllib2.install_opener(opener)
+#     html = urllib2.urlopen(url)
+#     return html
+#
+#
+# url = "http://www.csdn.net/"
+# proxies = ["101.53.101.172:9999", "171.117.93.229:8118", "119.251.60.37:21387", "58.246.194.70:8080"
+#                                                                                 "115.173.218.224:9797",
+#            "110.77.0.70:80"]
+# for i in range(0, 10000):
+#     try:
+#         html = getHtml(url, proxies)
+#         print
+#         html.info()  # 打印网页的头部信息，只是为了展示访问到了网页，可以自己修改成想显示的内容
+#         print
+#         i
+#     except:
+#         print
+#         "出现故障"
+
 url = "http://www.xgyw.cc/Rosimeimei/Rosimeimei7033.html"
 try:
     html = urllib.request.urlopen(url, timeout=5)
     response = html.read()
-except Exception as e:
-    print(e)
+except:
+    html = urllib.request.urlopen(url, timeout=5)
+    response = html.read()
 selector = etree.HTML(response)
 pages = selector.xpath('//div[@class="page"]//a//@href')
 lists = ['http://www.xgyw.cc' + x for x in pages]
@@ -19,8 +53,9 @@ for l in lists:
     try:
         html = urllib.request.urlopen(l,timeout=5)
         response = html.read()
-    except Exception as e:
-        print(e)
+    except:
+        html = urllib.request.urlopen(l, timeout=5)
+        response = html.read()
     selector = etree.HTML(response)
     content = selector.xpath('//div[@class="img"]//img//@src')
     contents = ['http://img.xgyw.cc' + x for x in content]
@@ -29,7 +64,7 @@ for l in lists:
         opener = urllib.request.build_opener()
         opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
         urllib.request.install_opener(opener)
-        urllib.request.urlretrieve(c, r'F:\test\rosi\%s' % name)
+        urllib.request.urlretrieve(c, r'E:\test\rosi\%s' % name)
         print(c)
         # os.chdir(r"F:\test\rosi")
         # header = {
